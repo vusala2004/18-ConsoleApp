1239ba0fa9663f33afbdc1d77ea2874067fd67bf
vusala <vusalavk-bpa203@code.edu.az>
2025-11-10 23:18:15 +0400

    yeni

--- Academy System/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Academy System/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46342fe7a258d12f1a0cbcb050bbecedf56ddb70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a382986bd4ac2c1d0fe8b6341e787cd980bcedd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
